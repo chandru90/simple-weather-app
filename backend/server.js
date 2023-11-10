a11cff9c786e5b662a44e3f5b6bf6cce8f8de5d7
chandru90 <iochandru90@gmail.com>
2023-11-10 16:13:22 +0530

    new commit

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,16 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const PORT = 5000;
+app.use(
+  cors({
+    origin: "https://simple-weather-app-mu-two.vercel.app",
+    methods: "POST,GET",
+    credentials: true
+  })
+);
 
 app.use(express.json());
+
 
 app.get('/weather/:city', async (req, res) => {
   const cityName = req.params.city;
